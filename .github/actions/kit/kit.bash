d7e6ad61ffdb16f073356496a0577a12f71bb412
LI Lei <i@lilei.tech>
2022-03-29 12:19:00 +0800

    Update and rename .github/actions/baseline/baseline.bash to .github/actions/kit/kit.bash

--- .github/actions/kit/kit.bash
+++ .github/actions/kit/kit.bash
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-[ -v _BASELINE_BASH ] && return # avoid duplicated source
-export _BASELINE_BASH # sourced sential
+[ -v _KIT_BASH ] && return # avoid duplicated source
+export _KIT_BASH # sourced sential
 
 # Group stdin to stdout with title
 # $1: group tile
 # https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#grouping-log-lines
-function bl::wf::group {
-  echo "::group::💬 $1"; cat; echo "::endgroup::"
+function kit::wf::group {
+  echo "::group::🎁 $1"; cat; echo "::endgroup::"
 }
 
 # Set stdin as value to environment with given name
 # $1: environment variable name
 # https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-environment-variable
-function bl::wf::env {
+function kit::wf::env {
   echo "$1<<__HEREDOC__" >> $GITHUB_ENV
   cat >> $GITHUB_ENV
   echo "__HEREDOC__" >> $GITHUB_ENV
@@ -23,14 +23,15 @@ function bl::wf::env {
 # $1: output name
 # https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-output-parameter
 # https://renehernandez.io/snippets/multiline-strings-as-a-job-output-in-github-actions/
-function bl::wf::output {
-  local val="$(< /dev/stdin)"
+function kit::wf::output {
+  local val
+  val="$(< /dev/stdin)"
   echo "::set-output name=$1::$val"
-  echo "$val" | bl::wf::group "set-output 👉 $1"
+  echo -n "$val" | kit::wf::group "📜 set-output name=$1"
 }
 
 # Flatten JSON to key-value lines
 #   $1: separator (default as ' 👉 ')
-function bl::json::flatten {
+function kit::json::flatten {
   jq -Mcr --arg sep "${1:- 👉 }" 'paths(type!="object" and type!="array") as $p | {"key":$p|join("."),"value":getpath($p)} | "\(.key)\($sep)\(.value|@json)"'
 }
